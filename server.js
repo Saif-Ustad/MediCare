b4cecc4aeed61a68d0518d2483163d754e57307d
Saif-Ustad <121380262+Saif-Ustad@users.noreply.github.com>
2023-04-14 17:01:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -732,6 +732,6 @@ app.post("/login", function (req, res) {
     })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("server has been started on port 3000");
 });
